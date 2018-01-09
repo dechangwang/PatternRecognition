597eb5713d3d49197be985ea9b8402664ab15442
shirleyDXL <shirley@tongji.edu.cn>
2018-01-09 10:46:10 +0800

    WIP

--- models/Knn.py
+++ models/Knn.py
@@ -1,8 +1,8 @@
-
 import numpy as np
-class KNN:
 
-    def __init__(self,k = 1):
+
+class KNN:
+    def __init__(self, k=1):
         self.k = k
 
     def fit(self, train_feature, train_label):
@@ -10,25 +10,23 @@ class KNN:
         self.train_feature = train_feature
         self.train_label = train_label
         return self
-    
-    def Knn(self, te_feature,tr_feature,tr_labels,k):
+
+    def Knn(self, te_feature, tr_feature, tr_labels, k):
         tr_num = tr_feature.shape[0]
-        a = np.tile(te_feature,(tr_num,1)) - tr_feature
-        b = a**2
-        c = b.sum(axis = 1)
-        d = c**0.5
+        a = np.tile(te_feature, (tr_num, 1)) - tr_feature
+        b = a ** 2
+        c = b.sum(axis=1)
+        d = c ** 0.5
         sortd = d.argsort()
         classCount = dict()
         for i in range(k):
             candidateLabel = tr_labels[sortd[i]]
-            classCount[candidateLabel] = classCount.get(candidateLabel,0) + 1
-        sortClassCount = sorted(classCount.iteritems(), key=classCount.get, reverse = True)
+            classCount[candidateLabel] = classCount.get(candidateLabel, 0) + 1
+        sortClassCount = sorted(classCount.iteritems(), key=classCount.get, reverse=True)
         return sortClassCount[0][0]
-    
+
     def predict(self, test_feature):
         pre = list()
         for i in range(test_feature.shape[0]):
-            pre.append(self.Knn(test_feature[i], self.train_feature,self.train_label,self.k))
+            pre.append(self.Knn(test_feature[i], self.train_feature, self.train_label, self.k))
         return pre
-    
-    
--- run_knn.py
+++ run_knn.py
@@ -1,33 +1,38 @@
 import pandas as pd
-import numpy as np
-import math as Math
-from preprocessing.grid import RoadGrid
+
 from models.Knn import *
+from preprocessing.grid import RoadGrid
+from utils import distance
+
+
 def divide_train_test(data):
-    data = data_raw.drop('EcNo_1',axis = 1)
+    data = data.drop('EcNo_1', axis=1)
 
     data_num = data.shape[0]
     data_perm = np.random.permutation(range(data_num))
     data = data.iloc[data_perm]
-    train_data = data[:int(data_num*0.8)]
-    test_data = data[int(data_num*0.8):]
+    train_data = data[:int(data_num * 0.8)]
+    test_data = data[int(data_num * 0.8):]
 
-    train_feature = np.array(train_data.ix[:,6:])
-    test_feature = np.array(test_data.ix[:,6:])
-    train_label = train_data.ix[:,4:6].astype(float)
-    test_label = test_data.ix[:,4:6].astype(float)
+    train_feature = np.array(train_data.ix[:, 6:])
+    test_feature = np.array(test_data.ix[:, 6:])
+    train_label = train_data.ix[:, 4:6].astype(float)
+    test_label = test_data.ix[:, 4:6].astype(float)
 
-    rg = RoadGrid(np.vstack((train_label,test_label)), 30)
+    rg = RoadGrid(np.vstack((train_label, test_label)), 30)
     train_label_ = rg.transform(np.array(train_label), False)
     test_label_ = np.array(test_label)
-    return train_feature,test_feature,train_label_,test_label_,rg
+    return train_feature, test_feature, train_label_, test_label_, rg
+
+
 def run():
     filename = './data/4g.csv'
     data_raw = pd.read_csv(filename)
-    train_feature,test_feature,train_label,test_label, rg = divide_train_test(data_raw)
+    train_feature, test_feature, train_label, test_label, rg = divide_train_test(data_raw)
     knn = KNN()
     knn.fit(train_feature, train_label)
     pred = knn.predict(test_feature)
     pre = np.array([rg.grid_center[idx] for idx in pred])
     error = [distance(pt1, pt2) for pt1, pt2 in zip(pre, test_label)]
+    print [np.min(error), np.max(error), np.mean(error), np.median(error)]
     return error
